382c5f918c79ca7e83fcb9853b976d798b4ef6a4
manuel <moncadajuanmanuel25@gmail.com>
2025-02-19 11:47:59 -0500

    suma

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,12 +10,7 @@ public class Main {
         System.out.println("hola mundo");
         System.out.println("mi mama me mima");
         System.out.println("ami a mi gata");
-        System.out.println("ami a mi gata");
-        System.out.println("ami a mi gata");
-        System.out.println("ami a mi gata");
-        System.out.println("ami a mi gata");
-        System.out.println("ami a mi gata");
-
+        System.out.println("suma");
 
 
 
